c3fa4dac91ae8ea0395d9ee6296e8225330e9d07
yutan <yutann082@gmail.com>
2023-01-14 10:29:13 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("ユーザーが接続しました");
+
+    socket.on("chat message", (msg) => {
+        // console.log("メッセージ:" + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("listening on 3000");
+});
